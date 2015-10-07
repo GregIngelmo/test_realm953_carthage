4c46ff1a5d7ab3fdd4d9e6fd517a26a33cec7df7
Greg Ingelmo <git@ingelmo.org>
2015-10-07 12:08:50 -0400

    Add osx info

--- test_carthage_build.sh
+++ test_carthage_build.sh
@@ -1,11 +1,15 @@
 #!/bin/sh
-echo "******************************************"
-echo Cathage version `carthage version`
+echo "OS X:"
+sw_vers
+echo "\nCathag:"
+carthage version
+echo "\nXcode:"
 xcodebuild -version
 xcrun swift -version
-echo "******************************************"
+echo ""
 
 # Clean Carthage dirs
+echo "Run Cathage"
 set -x 
 rm -fr Carthage 2> /dev/null
 rm -fr ~/Library/Caches/org.carthage.CarthageKit 2> /dev/null
